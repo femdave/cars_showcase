fb58dc83be42c125cc5e9cb6008b892f6fcddfbf
Ben <davidbenjamin048@gmail.com>
2024-04-16 23:32:16 +0100

    inital commit

---
+++ app/components/index.ts
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CarDetails from "./CarDetails";
+
+export {
+    Hero, 
+    CustomButton,
+    Navbar,
+    Footer,
+    CarDetails
+}
